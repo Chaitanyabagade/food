f438239a388205c0f9e4f1e481a540d4f2cf2196
chaitanya bagade <chaitanyabagade59@gmail.com>
2025-01-30 20:19:47 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // This ensures Tailwind scans all files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
